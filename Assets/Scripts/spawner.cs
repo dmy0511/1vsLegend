95dbd299969add7cfcc68f1862d2e8c1053755f5
dmy0511 <dmy0511love@gmail.com>
2023-06-29 08:59:24 +0900

    1.2

--- Assets/Scripts/spawner.cs
+++ Assets/Scripts/spawner.cs
@@ -4,38 +4,57 @@ using UnityEngine;
 
 public class spawner : MonoBehaviour
 {
-    public GameObject[] objectsToSpawn;
-    public float currentcooltime = 0.5f;
-
-    private float cooltime;
-    private float timer;
-
-    void Start()
+    [System.Serializable]
+    public class SpawnableObject
     {
-        cooltime = currentcooltime;
-        timer = cooltime;
+        public GameObject prefab;
+        public float initialSpeed;
     }
 
-    void Update()
+    public SpawnableObject[] objectsToSpawn;
+    public float currentCoolTime = 0.5f;
+    public float speedIncreaseRate = 0.1f;
+
+    private float coolTime;
+    private float timer;
+
+    private void Start()
+    {
+        coolTime = currentCoolTime;
+        timer = coolTime;
+    }
+
+    private void Update()
     {
         timer -= Time.deltaTime;
 
         if (timer <= 0f)
         {
             int randomIndex = Random.Range(0, objectsToSpawn.Length);
-            GameObject randomObject = objectsToSpawn[randomIndex];
+            SpawnableObject randomObject = objectsToSpawn[randomIndex];
 
-            GameObject spawnedObject = Instantiate(randomObject, transform.position, Quaternion.identity);
+            GameObject spawnedObject = Instantiate(randomObject.prefab, transform.position, Quaternion.identity);
+
+            Rigidbody2D rb = spawnedObject.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.velocity = Vector2.right * randomObject.initialSpeed;
+            }
 
             Renderer renderer = spawnedObject.GetComponent<Renderer>();
 
             Color randomColor = Random.Range(0, 2) == 0 ? Color.red : Color.blue;
             renderer.material.color = randomColor;
 
-            timer = cooltime;
+            timer = coolTime;
         }
 
-        cooltime = Mathf.Max(cooltime, 0f);
+        coolTime = Mathf.Max(coolTime, 0f);
+
+        foreach (SpawnableObject obj in objectsToSpawn)
+        {
+            obj.initialSpeed += speedIncreaseRate * Time.deltaTime;
+        }
     }
 }
 
