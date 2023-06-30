421af74c4879b08a904cbb08a096ed8719a1c17f
dmy0511 <127921600+dmy0511@users.noreply.github.com>
2023-07-01 00:10:34 +0900

    2.4

--- Assets/Scripts/ex02.cs
+++ Assets/Scripts/ex02.cs
@@ -9,17 +9,20 @@ public class ex02 : MonoBehaviour
 
     private bool isInsideBox = false;
     private bool BoxCenter = false;
-    
+
     private float moveSpeed = 5f;
 
     private void Start()
     {
+        // comScript와 userScript를 해당 타입의 인스턴스를 찾아 할당합니다.
         comScript = FindObjectOfType<com>();
         userScript = FindObjectOfType<user>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // 충돌한 오브젝트의 태그가 "Box"인 경우, isInsideBox를 true로 설정
+        // 충돌한 오브젝트의 태그가 "Center"인 경우, BoxCenter를 true로 설정
         if (collision.CompareTag("Box"))
         {
             isInsideBox = true;
@@ -32,6 +35,8 @@ public class ex02 : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        // 충돌한 오브젝트의 태그가 "Box"인 경우, isInsideBox를 false로 설정
+        // 충돌한 오브젝트의 태그가 "Center"인 경우, BoxCenter를 false로 설정
         if (collision.CompareTag("Box"))
         {
             isInsideBox = false;
@@ -44,26 +49,35 @@ public class ex02 : MonoBehaviour
 
     private void Update()
     {
+        // 현재 오브젝트의 Collider2D 컴포넌트와 Box의 Collider2D 컴포넌트를 가져옴
         Collider2D itemCollider = GetComponent<Collider2D>();
         Collider2D boxCollider = GameObject.FindGameObjectWithTag("Box").GetComponent<Collider2D>();
-        
+
+        Renderer renderer = GetComponent<Renderer>();
+
         if (BoxCenter)
         {
-            // Object is inside the box, stop moving
-            // Add any additional code you need for when the object is inside the box
+            // 오브젝트가 센터에 있으면 이동 X
+
+            Destroy(gameObject, 3f);
+
+            if (renderer.material.color == Color.blue)
+            {
+                userScript.DefenseFail(10);
+            }
         }
         else
         {
-            // Object is not inside the box, move from left to right at a constant speed
+            // 오브젝트가 센터 안에 없으면 일정한 속도로 오른쪽으로 이동
             Vector3 newPosition = transform.position + Vector3.right * moveSpeed * Time.deltaTime;
             transform.position = newPosition;
         }
 
         if (isInsideBox && Input.GetKeyDown(KeyCode.UpArrow))
         {
+            // 아이템 Collider가 상자 Collider 내에 포함되는지 확인
             if (boxCollider.bounds.Contains(itemCollider.bounds.min) && boxCollider.bounds.Contains(itemCollider.bounds.max))
             {
-                Renderer renderer = GetComponent<Renderer>();
 
                 if (renderer.material.color == Color.red)
                 {
@@ -75,9 +89,9 @@ public class ex02 : MonoBehaviour
         }
         else if (isInsideBox && (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)))
         {
+            // 아이템 Collider가 상자 Collider 내에 포함되는지 확인
             if (boxCollider.bounds.Contains(itemCollider.bounds.min) && boxCollider.bounds.Contains(itemCollider.bounds.max))
             {
-                Renderer renderer = GetComponent<Renderer>();
 
                 if (renderer.material.color == Color.red)
                 {
