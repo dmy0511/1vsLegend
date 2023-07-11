4fa69968f84f6edc85e5542d44105b80e2260dc0
dmy0511 <dmy0511love@gmail.com>
2023-07-11 10:45:28 +0900

    dkdkkddkdk

--- Assets/Scripts/Moving/UserMoving.cs
+++ Assets/Scripts/Moving/UserMoving.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class UserMovement : MonoBehaviour
+public class UserMoving : MonoBehaviour
 {
-    public float normalSpeed = 3f;
+    public float normalSpeed = 1f;
     public float currentSpeed;
 
     public string nextSceneName;
@@ -19,13 +19,20 @@ public class UserMovement : MonoBehaviour
     {   // 자동으로 왼쪽에서 오른쪽으로 이동
         Vector3 movement = new Vector3(-currentSpeed, 0f, 0f);
         transform.Translate(movement * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            currentSpeed += 1f;
+        }
+
+        currentSpeed = Mathf.Clamp(currentSpeed, normalSpeed, float.MaxValue);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Box"))
         {
-            SceneManager.LoadScene(nextSceneName);
+            normalSpeed = 0;
         }
     }
 }
